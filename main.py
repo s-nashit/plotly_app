d4547ee1471d53961644a7d286a6c1939e7a550d
Nashit Humam <85383203+s-nashit@users.noreply.github.com>
2025-03-26 11:50:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+data = {
+    'area' : [100,200,300,400,500,600,700,750,800,900,1000],
+    'cost' : [98, 150, 200, 250, 300, 350, 412, 459, 507, 567, 600]
+}
+
+df = pd.DataFrame(data)
+
+app.layout = html.Div([html.H1('My Application'),
+html.H2('Area vs Cost Graph'),
+dcc.Dropdown(
+    id = 'graph_type',
+    options = [
+        {'label': 'Line Plot', 'value' : 'line'},
+        {'label': 'Bar Plot', 'value' : 'bar'},
+        {'label': 'Scatter Plot', 'value' : 'scatter'}
+    ]
+),
+dcc.Graph(id = 'graph')],
+                      style={'textAlign': 'center',
+                             'color': 'red',
+                             'backgroundColor': 'lightblue'})
+
+
+@app.callback(
+    Output('graph', 'figure'),
+    [Input('graph_type', 'value')]
+)
+
+def update_graph(graph_type):
+    if graph_type == 'line':
+        fig = px.line(df, x = 'area', y='cost')
+    elif graph_type == 'bar':
+        fig = px.bar(df, x = 'area', y='cost')
+    else:
+        fig = px.scatter(df, x = 'area', y='cost')
+    
+    return fig
+
+
+app.run(debug=True)
